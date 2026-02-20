f5f2d5558f3f9b6c23c801d938e23dfa21bcb9a5
lillie <onlinechronically@protonmail.com>
2026-02-20 09:25:51 -0500

    attempt-add: type checker/interpreter

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{io, iter::Peekable, str::Chars, vec::IntoIter};
+use std::{collections::HashMap, io, iter::Peekable, str::Chars, vec::IntoIter};
 
 use anyhow::{Result, bail};
 
@@ -38,6 +38,81 @@ enum TypeError {
 enum CheckedType {
     Type(Type),
     Error(TypeError),
+}
+
+impl Node {
+    fn type_check_env(&self, env: &mut HashMap<String, Type>) -> CheckedType {
+        match self {
+            Node::Number(_) => CheckedType::Type(Type::Number),
+            Node::Var(sym) => {
+                if env.contains_key(sym) {
+                    CheckedType::Type(env[sym].clone())
+                } else {
+                    CheckedType::Error(TypeError::UnboundVariable)
+                }
+            }
+            // Node::Lambda(sym, body) => match body.type_check_env(env) {
+            //     CheckedType::Type(t) => {
+            //         env.insert(sym, v)
+            //     }
+            //     CheckedType::Error(err) => CheckedType::Error(err),
+            // },
+            Node::Application(rator, rand) => {
+                match (rator.type_check_env(env), rand.type_check_env(env)) {
+                    (CheckedType::Type(rator_type), CheckedType::Type(rand_type)) => {
+                        match rator_type {
+                            Type::Function(inp, out) => {
+                                if *inp == rand_type {
+                                    CheckedType::Type(*out)
+                                } else {
+                                    CheckedType::Error(TypeError::FunctionInputMismatch)
+                                }
+                            }
+                            _ => CheckedType::Error(TypeError::NonFunctionApplication(rator_type)),
+                        }
+                    }
+                    (CheckedType::Error(rator_error), CheckedType::Error(rand_error)) => {
+                        CheckedType::Error(TypeError::CascadeDouble(
+                            Box::new(rator_error),
+                            Box::new(rand_error),
+                        ))
+                    }
+                    (_, CheckedType::Error(rand_error)) => {
+                        CheckedType::Error(TypeError::Cascade(Box::new(rand_error)))
+                    }
+                    (CheckedType::Error(rator_error), _) => {
+                        CheckedType::Error(TypeError::Cascade(Box::new(rator_error)))
+                    }
+                }
+            }
+            Node::Lambda(sym, sym_type, body) => {
+                env.insert(sym.clone(), sym_type.clone());
+                match body.type_check_env(env) {
+                    CheckedType::Type(t) => {
+                        CheckedType::Type(Type::Function(Box::new(sym_type.clone()), Box::new(t)))
+                    }
+                    CheckedType::Error(err) => CheckedType::Error(err),
+                }
+            }
+        }
+    }
+    fn type_check(&self) -> CheckedType {
+        self.type_check_env(&mut HashMap::new())
+    }
+    // fn interp_env(&self, env: HashMap<String, Returnable>) -> Returnable {
+    //     match self {
+    //         Node::Number(n) => Returnable::Number(*n),
+    //         Node::Lambda(sym, body) => Returnable::Lambda(self.clone()),
+    //         Node::Var(sym) => match env.get(sym) {
+    //             Some(v) => {}
+    //             None => {}
+    //         },
+    //         Node::Lambda(sym, body) => {}
+    //     }
+    // }
+    // fn interp(&self) -> Returnable {
+    //     self.interp_env(HashMap::new())
+    // }
 }
 
 #[derive(Debug)]
