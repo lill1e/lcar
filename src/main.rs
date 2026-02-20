35aafc144194f97cec2168b535ffbef65f7107f9
lillie <onlinechronically@protonmail.com>
2026-02-20 09:25:42 -0500

    add: Parser

--- src/main.rs
+++ src/main.rs
@@ -39,6 +39,7 @@ enum CheckedType {
     Type(Type),
     Error(TypeError),
 }
+
 #[derive(Debug)]
 enum Token {
     Identifier(String),
@@ -117,6 +118,47 @@ fn lex_lc(program: String) -> Result<Vec<Token>> {
     Ok(tokens)
 }
 
+fn consume_identifier(tokens: &mut Peekable<IntoIter<Token>>) -> Result<String> {
+    if let Some(Token::Identifier(id)) = tokens.next_if(|tok| matches!(tok, Token::Identifier(_))) {
+        Ok(id)
+    } else {
+        bail!("consume type mismatch")
+    }
+}
+
+fn consume_type(tokens: &mut Peekable<IntoIter<Token>>) -> Result<Type> {
+    if let Some(Token::TypeAnnotation(t)) =
+        tokens.next_if(|tok| matches!(tok, Token::TypeAnnotation(_)))
+    {
+        Ok(t)
+    } else {
+        bail!("consume type mismatch")
+    }
+}
+
+fn parse_literal(tokens: &mut Peekable<IntoIter<Token>>) -> Result<Node> {
+    match tokens.next_if(|tok| matches!(tok, Token::Number(_) | Token::Lambda)) {
+        Some(Token::Number(n)) => Ok(Node::Number(n)),
+        Some(Token::Lambda) => {
+            let id = consume_identifier(tokens);
+            let t = consume_type(tokens);
+            Ok(Node::Lambda(id?, t?, Box::new(parse_top(tokens)?)))
+        }
+        Some(Token::Identifier(ident)) => Ok(Node::Var(ident)),
+        Some(Token::LeftParen | Token::RightParen | Token::TypeAnnotation(_)) | None => {
+            bail!("expected literal")
+        }
+    }
+}
+
+fn parse_top(tokens: &mut Peekable<IntoIter<Token>>) -> Result<Node> {
+    Ok(parse_literal(tokens)?)
+}
+
+fn parse(tokens: Vec<Token>) -> Result<Node> {
+    Ok(parse_top(&mut tokens.into_iter().peekable())?)
+}
+
 fn main() -> Result<()> {
     let mut in_str = String::new();
     let mut temp_str = String::new();
@@ -130,5 +172,7 @@ fn main() -> Result<()> {
     }
     let l = lex_lc(in_str)?;
     dbg!(&l);
+    let p = parse(l);
+    dbg!(p?);
     Ok(())
 }
