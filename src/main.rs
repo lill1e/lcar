8f2ee0efa38087f8b73b1cc1af4628968ca20417
lillie <onlinechronically@protonmail.com>
2026-03-09 19:14:27 -0400

    fix: conflict

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,9 @@ mod lexer;
 mod parser;
 mod type_check;
 
-use std::io;
-
+use crate::{lexer::lex_lc, parser::parse, type_check::CheckedType};
 use anyhow::Result;
-
-use crate::{lexer::lex_lc, parser::parse};
+use std::io;
 
 fn main() -> Result<()> {
     let mut in_str = String::new();
@@ -22,7 +20,15 @@ fn main() -> Result<()> {
     }
     let l = lex_lc(in_str)?;
     dbg!(&l);
-    let p = parse(l);
-    dbg!(p?);
+    let p = parse(l)?;
+    dbg!(&p);
+    let type_check_result = p.clone().type_check();
+    match type_check_result {
+        CheckedType::Type(program_type) => {
+            println!("Program Type via Type Checker: {}", program_type);
+        }
+        CheckedType::Error(err) => println!("The program failed to type check: {}", err),
+    }
+    println!("Program Result via Interpretation: {}", p.interp());
     Ok(())
 }
